516cc63f0d487a93a13baaaef2895242b61e7154
Whavi <ihtef_st@hotmail.com>
2023-06-14 11:51:24 +0200

    problem fix attribution

--- src/Form/UserFormAttributionType.php
+++ src/Form/UserFormAttributionType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Attribution;
 use App\Entity\Collaborateur;
 use App\Entity\Product;
+use App\Repository\CollaborateurRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -26,6 +28,10 @@ class UserFormAttributionType extends AbstractType
                 'label_attr' => [
                     'class' => 'form_label mt-4'
                 ],
+                'query_builder' => function (CollaborateurRepository $cr) {
+                    return $cr->createQueryBuilder('c')
+                        ->orderBy('c.nom', 'ASC');
+                    },
                 'placeholder' => 'Choisissez un collaborateur',
                 'required' => true,   ]
             )
@@ -37,6 +43,10 @@ class UserFormAttributionType extends AbstractType
                 'label_attr' => [
                     'class' => 'form_label mt-4'
                 ],
+                'query_builder' => function (ProductRepository $pr) {
+                    return $pr->createQueryBuilder('p')
+                        ->orderBy('p.category', 'ASC');
+                    },
                 'placeholder' => 'Choisissez un produit',
                 'required' => true, ]
             )
