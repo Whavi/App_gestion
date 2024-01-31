5468c6a44723abb09396e1518e4944b750b084ab
whavi <ihtef_st@hotmail.com>
2024-01-31 12:16:10 +0100

    new branch

--- src/Entity/Attribution.php
+++ src/Entity/Attribution.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+
+// #[ORM\Table(name: 'sif_products')]
 #[ORM\Entity(repositoryClass: AttributionRepository::class)]
 class Attribution
 {
