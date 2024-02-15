9bbcd5cfbed735f3456fcddb3de4aad52ca53cd0
whavi <ihtef_st@hotmail.com>
2024-02-15 12:17:02 +0100

    User Log in database

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,6 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Psr\Log\LoggerInterface;
+use App\Entity\LogEntry;
 
 
 
@@ -28,7 +29,7 @@ class UserController extends AbstractController
 ############################################################################################################################
 #[Route('/gestion/compte/utilisateur', name: 'user_gestion_utilisateur')]
 #[IsGranted('ROLE_USER')]
-public function gestion_cpt_utilisateur(LoggerInterface $logger, UserRepository $UserRepository, Request $request, PaginatorInterface $paginatorInterface) {
+public function gestion_cpt_utilisateur(LoggerInterface $logger, UserRepository $UserRepository,PersistenceManagerRegistry $doctrine, Request $request, PaginatorInterface $paginatorInterface) {
     $users = $UserRepository->findAllOrderedByRank();
     $posts = $paginatorInterface->paginate($users, $request->query->getInt('page', 1), 12);
     $searchDataUser = new SearchDataUser();
@@ -37,14 +38,14 @@ public function gestion_cpt_utilisateur(LoggerInterface $logger, UserRepository 
         if($form->isSubmitted() && $form->isValid()){
             $data = $UserRepository->findAllOrderedByNameUser($searchDataUser);
             $posts = $paginatorInterface->paginate($data, $request->query->getInt('page', 1), 12);
-            $this->processUserRecherche($logger, $searchDataUser);
+            $this->processUserRecherche($logger, $doctrine,$searchDataUser);
             return $this->render('pages/user/utilisateur.html.twig', [
                 'form' => $form->createView(),
                 'users' => $posts, 
                 ],
             );
         }
-    $this->processUserAccueil($request, $logger);
+    $this->processUserAccueil($request, $doctrine,$logger);
     return $this->render('pages/user/utilisateur.html.twig', [
         'form' => $form->createView(),
         'users' => $posts,
@@ -68,15 +69,15 @@ public function gestionUserDelete($id,LoggerInterface $logger,  UserRepository $
 ############################################################################################################################
 #[Route('/gestion/compte/utilisateur/edit/{id}', name: 'user_gestion_utilisateur_edit')]
 #[IsGranted('ROLE_ADMIN')]
-public function gestionUserEdit($id,LoggerInterface $logger, UserRepository $userRepository, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $userPasswordHasher) : Response {
+public function gestionUserEdit($id,LoggerInterface $logger, UserRepository $userRepository,PersistenceManagerRegistry $doctrine, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $userPasswordHasher) : Response {
     $utilisateur = $userRepository->find($id);
     $form = $this->createForm(EditFormUserType::class, $utilisateur);
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()){
-        $this->processUserEdit($utilisateur,$form->getData(),$manager,$userPasswordHasher->hashPassword( $utilisateur, $form->get('password')->getData()),$logger);
+        $this->processUserEdit($utilisateur,$form->getData(),$manager,$userPasswordHasher->hashPassword( $utilisateur, $form->get('password')->getData()),$doctrine,$logger);
         return $this->redirectToRoute('user_gestion_utilisateur');
     }
-    $this->processUserEditEntry($utilisateur, $logger);
+    $this->processUserEditEntry($utilisateur,$doctrine, $logger);
     return $this->render('pages/user/edit/editUser.html.twig', [
         'utilisateur' => $utilisateur,
         'form' => $form->createView()
@@ -88,19 +89,19 @@ public function gestionUserEdit($id,LoggerInterface $logger, UserRepository $use
 ############################################################################################################################
 #[Route('/gestion/compte/utilisateur/addUser', name: 'user_gestion_newItemUser')]
 #[IsGranted('ROLE_ADMIN')]
-public function addItemUser(LoggerInterface $logger, EntityManagerInterface $manager, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+public function addItemUser(LoggerInterface $logger, EntityManagerInterface $manager, PersistenceManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
 {
     $form = $this->createForm(UserFormItemType::class);
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
         $userPasswordHashed = $userPasswordHasher->hashPassword($form->getData(), $form->get('password')->getData());
-        $this->processUserCreate($form->getData(), $manager, $userPasswordHashed, $logger);
+        $this->processUserCreate($form->getData(), $manager, $userPasswordHashed, $doctrine,$logger);
 
         $this->addFlash('success', 'Votre compte a bien été créé.');
         return $this->redirectToRoute('user_gestion_utilisateur');
     }
-    $this->processUserCreateEntry($logger);
+    $this->processUserCreateEntry($doctrine,$logger);
     return $this->render('pages/user/newItem/User.html.twig', [
         'form' => $form->createView()
     ]);
@@ -119,8 +120,30 @@ public function addItemUser(LoggerInterface $logger, EntityManagerInterface $man
 ######################################################   FONCTION PRIVÉE   #################################################
 ############################################################################################################################
 
-private function processUserAccueil($request, $logger){
+private function logToDatabase(string $message, array $context = [], $channel,  ?PersistenceManagerRegistry $doctrine = null, $level = 1 ): void
+    {
+         // Merge context parameters into the message
+         foreach ($context as $key => $value) {
+            $message = str_replace("{{$key}}", $value, $message);
+        }
+
+        $logEntry = new LogEntry();
+        $logEntry->setMessage($message);
+        $logEntry->setCreatedAt(new \DateTime());
+        $logEntry->setChannel($channel);
+        $logEntry->setLevel($level);
+        
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($logEntry);
+        $entityManager->flush();
+    }
+
+private function processUserAccueil($request,$doctrine, $logger){
     $page = $request->query->getInt('page', 1);
+    $this->logToDatabase("{user} est rentré dans la page $page d'accueil Utilisateur", [
+        'user' => $this->getUser(),
+    ],"USER",$doctrine);
     $logger->info("{user} est rentré dans la page $page d'accueil Utilisateur | heure => {date}", [
         'user' => $this->getUser(),
         'date' => (new \DateTime())->format('d/m/Y H:i:s'),
@@ -128,7 +151,11 @@ private function processUserAccueil($request, $logger){
 } 
 
 
-private function processUserRecherche($logger, $searchDataUser){
+private function processUserRecherche($logger,$doctrine, $searchDataUser){
+    $this->logToDatabase("{user} fait une recherche dans la page Utilisateur | recherche => {rech}", [
+        'user' => $this->getUser(),
+        'rech' => $searchDataUser->getRecherche(),
+    ],"USER",$doctrine);
     $logger->info("{user} fait une recherche dans la page Utilisateur | recherche => {rech} | heure => {date}", [
         'user' => $this->getUser(),
         'rech' => $searchDataUser->getRecherche(),
@@ -137,10 +164,12 @@ private function processUserRecherche($logger, $searchDataUser){
 }
 
 private function processUserDelete($user, $id, $manager, $doctrine, $logger){
-    $this->addFlash('success',"L'utilisateur a été supprimer");
-    $manager = $doctrine->getManager();
-    $manager->remove($user);
-    $manager->flush();
+    $this->logToDatabase("{user} a supprimer l'utilisateur {utilisateur} | Email => {mail} | id => {id}", [
+        'id'=> $id,
+        'user'=>$this->getUser(),
+        'utilisateur'=>strtoupper($user->getNom()). " ".$user->getPrenom(),
+        'mail'=>$user->getEmail(),
+        ],"USER",$doctrine);
     $logger->info("{user} a supprimer l'utilisateur {utilisateur} | Email => {mail} | id => {id} | heure de suppréssion => {date}", [
         'id'=> $id,
         'user'=>$this->getUser(),
@@ -149,24 +178,35 @@ private function processUserDelete($user, $id, $manager, $doctrine, $logger){
         'date'=>(new \DateTime())->format('d/m/Y H:i:s'),
         ]);
 
+
+    $manager = $doctrine->getManager();
+    $manager->remove($user);
+    $manager->flush();
+    $this->addFlash('success',"L'utilisateur a été supprimer");
+
     }
 
-private function processUserEdit($user, $data, $manager,$userPasswordHashed, $logger){
-    $this->addFlash('success', 'Votre Utilisateur a bien été modifié.');
+private function processUserEdit($user, $data, $manager,$userPasswordHashed,$doctrine, $logger){
 
     $user->setPassword($userPasswordHashed);
     $manager->persist($data);
     $manager->flush();
 
+    $this->logToDatabase("{user} à modifier l'utilisateur : {utilisateur} | email => {mail}", [
+        'user'=>$this->getUser(),
+        'utilisateur'=>strtoupper($user->getNom()). " ".$user->getPrenom(),
+        'mail'=>$user->getEmail(),
+    ],"USER",$doctrine);
     $logger->info("{user} à modifier l'utilisateur : {utilisateur} | email => {mail} | heure de changement : {date}", [
         'user'=>$this->getUser(),
         'utilisateur'=>strtoupper($user->getNom()). " ".$user->getPrenom(),
         'mail'=>$user->getEmail(),
         'date'=>(new \DateTime())->format('d/m/Y H:i:s'),
         ]);
+    $this->addFlash('success', 'Votre Utilisateur a bien été modifié.');
     }
 
-private function processUserCreate($data, $manager, $userPasswordHashed, $logger)
+private function processUserCreate($data, $manager, $userPasswordHashed, $doctrine,$logger)
 {
     $userItem = new User();
     $userItem->setNom($data->getNom());
@@ -177,6 +217,12 @@ private function processUserCreate($data, $manager, $userPasswordHashed, $logger
     $manager->persist($userItem);
     $manager->flush();
 
+    $this->logToDatabase("{user} a créé un Utilisateur => {utilisateur} | email => {mail} | rôles => {roles}", [
+        'user' => $this->getUser(),
+        'utilisateur' => strtoupper($userItem->getNom()) . " " . $userItem->getPrenom(),
+        'mail' => $userItem->getEmail(),
+        'roles' => implode(', ', $userItem->getRoles()),
+    ],"USER",$doctrine);
     $logger->info("{user} a créé un Utilisateur => {utilisateur} | email => {mail} | rôles => {roles} | heure de création : {date}", [
         'user' => $this->getUser(),
         'utilisateur' => strtoupper($userItem->getNom()) . " " . $userItem->getPrenom(),
@@ -185,7 +231,12 @@ private function processUserCreate($data, $manager, $userPasswordHashed, $logger
         'date' => (new \DateTime())->format('d/m/Y H:i:s'),
     ]);
 }
-private function processUserEditEntry($user,$logger){
+private function processUserEditEntry($user,$doctrine,$logger){
+    $this->logToDatabase("{user} est rentré dans la page d'édition de l'Utilisateur {utilisateur} | email => {mail}", [
+        'user'=>$this->getUser(),
+        'utilisateur'=>strtoupper($user->getNom()). " ".$user->getPrenom(),
+        'mail'=>$user->getEmail(),
+   ],"USER",$doctrine);
     $logger->info("{user} est rentré dans la page d'édition de l'Utilisateur {utilisateur} | email => {mail} | heure => {date}", [
         'user'=>$this->getUser(),
         'utilisateur'=>strtoupper($user->getNom()). " ".$user->getPrenom(),
@@ -194,7 +245,10 @@ private function processUserEditEntry($user,$logger){
    ]);
 }
 
-private function processUserCreateEntry($logger){
+private function processUserCreateEntry($doctrine,$logger){
+    $this->logToDatabase("{user} est rentré dans la page d'ajout de l'Utilisateur", [
+        'user'=>$this->getUser(),
+   ],"USER",$doctrine);
     $logger->info("{user} est rentré dans la page d'ajout de l'Utilisateur | heure => {date}", [
         'user'=>$this->getUser(),
         'date'=>(new \DateTime())->format('d/m/Y H:i:s'),
