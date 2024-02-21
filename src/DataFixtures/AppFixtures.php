f2d2577b005527db5245d1c6542fceeee3193e76
whavi <ihtef_st@hotmail.com>
2024-02-21 13:39:27 +0100

    deprecation fixture fixed

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,28 +6,34 @@ use App\Entity\User;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AppFixtures extends Fixture
 {
+    private $container;
 
-   
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // Users
         UserFactory::createMany(5);
+        
         // Admin 
         $users = [];
 
         $admin = new User();
         $admin->setNom('Bouanane')
             ->setPrenom("Fethi")
-            ->SetEmail('admin@admin.org')
+            ->setEmail('admin@admin.org')
             ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
             ->setPassword('$2y$13$mk0lFLjyJ7m2HOHEgQhdleQIjVo2YodvAIZqnMR3//Am5XLdW4Swu');
 
         $users[] = $admin;
         $manager->persist($admin);
         $manager->flush();
-        
     }
 }
--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Product;
 use App\Factory\ProductFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
