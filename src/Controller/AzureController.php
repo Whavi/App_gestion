e39abdb140b8c26c323032584a8459b1cb760b02
whavi <ihtef_st@hotmail.com>
2024-02-22 10:06:31 +0100

    Guard Oauth Azure

--- src/Controller/AzureController.php
+++ src/Controller/AzureController.php
@@ -10,36 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AzureController extends AbstractController
 {
-    /**
-     * Link to this controller to start the "connect" process
-     *
-     * @Route("/connect/azure", name="connect_azure_start")
-     */
+
     #[Route('/connect/azure', name: 'connect_azure_start')]    
-    #[IsGranted('ROLE_USER')]
     public function connectAction(ClientRegistry $clientRegistry)
     {
         // will redirect to Azure!
         return $clientRegistry
-            ->getClient('azure') // key used in config/packages/knpu_oauth2_client.yaml
-            ->redirect();
+            ->getClient('azure')
+            ->redirect(["openid", "profile", "email"], []);
     }
 
-    /**
-     * After going to Azure, you're redirected back here
-     * because this is the "redirect_route" you configured
-     * in config/packages/knpu_oauth2_client.yaml
-     *
-     * @Route("/connect/azure/check", name="connect_azure_check")
-     */
     #[Route('/connect/azure/check', name: 'connect_azure_check')]
-    #[IsGranted('ROLE_USER')]
     public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
     {
-        // ** if you want to *authenticate* the user, then
-        // leave this method blank and create a Guard authenticator
-        // (read below)
-
         /** @var \KnpU\OAuth2ClientBundle\Client\Provider\AzureClient $client */
         $client = $clientRegistry->getClient('azure');
 
@@ -53,8 +36,6 @@ class AzureController extends AbstractController
             var_dump($user); die;
             // ...
         } catch (IdentityProviderException $e) {
-            // something went wrong!
-            // probably you should return the reason to the user
             var_dump($e->getMessage()); die;
         }
     }
--- src/Security/MyAzureAuthenticator.php
+++ src/Security/MyAzureAuthenticator.php
@@ -4,94 +4,100 @@ namespace App\Security;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use KnpU\OAuth2ClientBundle\Client\Provider\AzureClient;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-class MyAzureAuthenticator extends OAuth2Authenticator implements AuthenticationEntryPointInterface
+class MyFacebookAuthenticator extends SocialAuthenticator
 {
     private $clientRegistry;
-    private $entityManager;
+    private $em;
     private $router;
 
-    public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $entityManager, RouterInterface $router)
+    public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $em, RouterInterface $router)
     {
         $this->clientRegistry = $clientRegistry;
-        $this->entityManager = $entityManager;
+        $this->em = $em;
         $this->router = $router;
     }
 
-    public function supports(Request $request): ?bool
+    public function supports(Request $request)
     {
         // continue ONLY if the current ROUTE matches the check ROUTE
         return $request->attributes->get('_route') === 'connect_azure_check';
     }
 
-    public function authenticate(Request $request): Passport
+    public function getCredentials(Request $request)
     {
-        $client = $this->clientRegistry->getClient('azure');
-        $accessToken = $this->fetchAccessToken($client);
-
-        return new SelfValidatingPassport(
-            new UserBadge($accessToken->getToken(), function () use ($accessToken, $client) {
-                // Implement your Azure user fetching logic here
-                // ...
-
-                // Example code assuming you have an Azure user ID
-                $azureUserId = '...'; // Replace with your logic to get Azure user ID
-
-                // 1) have they logged in with Azure before? Easy!
-                $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['azureId' => $azureUserId]);
-
-                if ($existingUser) {
-                    return $existingUser;
-                }
-
-                // 2) do we have a matching user by Azure ID?
-                // Adjust this part based on your User entity and Azure user data
-                $user = new User();
-                $user->setAzureId($azureUserId);
-                $this->entityManager->persist($user);
-                $this->entityManager->flush();
-
-                return $user;
-            })
-        );
+        return $this->fetchAccessToken($this->getAzureClient());
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        /** @var User $AzureUser */
+        $AzureUser = $this->getAzureClient()
+            ->fetchUserFromToken($credentials);
+
+        $email = $AzureUser->getEmail();
+
+        // 1) have they logged in with Facebook before? Easy!
+        $existingUser = $this->em->getRepository(User::class)
+            ->findOneBy(['facebookId' => $AzureUser->getId()]);
+        if ($existingUser) {
+            return $existingUser;
+        }
+
+        // 2) do we have a matching user by email?
+        $user = $this->em->getRepository(User::class)
+            ->findOneBy(['email' => $email]);
+
+        // 3) Maybe you just want to "register" them by creating
+        // a User object
+        $user->setAzureId($AzureUser->getId());
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $user;
+    }
+
+    /**
+     * @return AzureClient
+     */
+    private function getAzureClient()
+    {
+          /** @var AzureClient $client */
+          $client = $this->clientRegistry->getClient('azure');
+
+          return $client;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         // change "app_homepage" to some route in your app
-        $targetUrl = $this->router->generate('app_homepage');
+        $targetUrl = $this->router->generate('user_gestion_attribution');
 
         return new RedirectResponse($targetUrl);
+    
+        // or, on success, let the request continue to be handled by the controller
+        //return null;
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         $message = strtr($exception->getMessageKey(), $exception->getMessageData());
 
         return new Response($message, Response::HTTP_FORBIDDEN);
     }
-
-    /**
-     * Called when authentication is needed, but it's not sent.
-     * This redirects to the 'login'.
-     */
-    public function start(Request $request, AuthenticationException $authException = null): Response
+    public function start(Request $request, AuthenticationException $authException = null)
     {
-        return new RedirectResponse(
-            '/connect/', // might be the site, where users choose their oauth provider
-            Response::HTTP_TEMPORARY_REDIRECT
-        );
+        return new RedirectResponse( '/connect/', Response::HTTP_TEMPORARY_REDIRECT);
     }
+
 }
